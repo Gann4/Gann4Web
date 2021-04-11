8c64fa8ac49a58147c3b0a9c43eca3864ccdab61
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-04-11 04:38:54 -0300

    Unsafe action

--- sites/contact.php
+++ sites/contact.php
@@ -12,9 +12,15 @@
     <div class="content centered">
         <h1 class="content-title">Contact</h1>
         <form action="" method="post">
+<<<<<<< HEAD
             <input type="email" name="email" id="" placeholder="youremail@example.com">
             <input type="subject" name="subject" id="" placeholder="Subject">
             <textarea id="message" name="message" placeholder="Type your message here."></textarea>
+=======
+            <input type="email" name="email" id="" value="youremail@example.com">
+            <input type="text" name="subject" id="" value="Subject">
+            <input type="text" name="message" id="" value="Enter your message here.">
+>>>>>>> parent of c5e58d1 (Working contact section)
             <input type="submit" value="Send">
         </form>
     </div>
